b860884dc6da3d8e3a2aadae2670b504d13a2ca9
a.hayat <a.hayat@FRPA-LPT-035.itecor.ch>
2021-06-17 19:39:57 +0200

    le premier commit

---
+++ src/Package1/Test1.java
@@ -0,0 +1,17 @@
+package Package1;
+
+public class Test1 {
+
+	public static void main(String[] args) {
+		
+String Hungry = "yes";
+if (Hungry == "yes")
+System.out.println("est something......");
+else {
+	System.out.println("Do work......");
+	
+}
+
+	}
+
+}
